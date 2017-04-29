d0aacb2dacc15229270d66ec3c819ebfd6c1b71a
Brian Du <brian.bh.du@gmail.com>
2017-04-29 19:18:54 -0400

    Add on/off click sound

--- js/main.js
+++ js/main.js
@@ -2,7 +2,15 @@ function onoff() {
   currentvalue = document.getElementById('onoff').value;
   if(currentvalue == "Off"){
     document.getElementById("onoff").value="On";
+    clickOn.play();
   }else{
     document.getElementById("onoff").value="Off";
+    clickOff.play();
   }
 }
+
+var clickOn = new Audio();
+clickOn.src = "http://soundbible.com/mp3/Click On-SoundBible.com-1697535117.mp3";
+
+var clickOff = new Audio();
+clickOff.src = "http://soundbible.com/mp3/Button Click Off-SoundBible.com-1730098776.mp3";
