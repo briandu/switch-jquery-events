e31bfb57930cfda81ca11795d73a34fd126c276e
Brian Du <brian.bh.du@gmail.com>
2017-04-29 18:49:50 -0400

    Add on/off state of button

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
-
-
+function onoff() {
+  currentvalue = document.getElementById('onoff').value;
+  if(currentvalue == "Off"){
+    document.getElementById("onoff").value="On";
+  }else{
+    document.getElementById("onoff").value="Off";
+  }
+}
